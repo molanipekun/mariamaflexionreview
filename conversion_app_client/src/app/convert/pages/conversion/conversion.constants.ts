d4015b32cacf218cabd08fa416581753f1d99265
MariamaO <molanipekun@outlook.com>
2020-08-31 14:37:11 +0100

    Docker File Addition

--- conversion_app_client/src/app/convert/pages/conversion/conversion.constants.ts
+++ conversion_app_client/src/app/convert/pages/conversion/conversion.constants.ts
@@ -8,53 +8,6 @@ export const VOLUME_TYPES = [
     'liters', 'tablespoons', 'cubic-inches', 'cups', 'cubic-feets', 'gallons'
 ];
 
-// export const TEMPERATURE_MAP: IUnit[] = [
-//     {
-//         unit: "kelvin",
-//         symbol: "K"
-//     },
-//     {
-//         unit: "celsius",
-//         symbol: "C"
-//     },
-//     {
-//         unit: "fahrenheit",
-//         symbol: "F"
-//     },
-//     {
-//         unit: "rankine",
-//         symbol: "R"
-//     }
-// ];
-
-
-// export const VOLUME_MAP: IUnit[] = [
-//     {
-//         unit: "liters",
-//         symbol: "l"
-//     },
-//     {
-//         unit: "tablespoons",
-//         symbol: "tsp"
-//     },
-//     {
-//         unit: "cubic-inches",
-//         symbol: "in3"
-//     },
-//     {
-//         unit: "cups",
-//         symbol: "cup"
-//     },
-//     {
-//         unit: "cubic-feets",
-//         symbol: "ft3"
-//     },
-//     {
-//         unit: "gallons",
-//         symbol: "gal"
-//     }
-// ]
-
 export const ALL_UNIT_TYPES = {
     temperature: [...TEMPERATURE_TYPES],
     volume: [...VOLUME_TYPES]
